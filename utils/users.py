28ef84f3c7c4db9fc8c4bb6fac5fc0a70dd2d9c3
Rutty56 <98373154+Rutty56@users.noreply.github.com>
2025-04-22 15:39:42 +0700

    Update users.py

--- utils/users.py
+++ utils/users.py
@@ -1,1 +1,20 @@
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+REGISTERED_USERS_FILE = "registered_users.txt"
+
+def save_registered_user(user_id):
+    if os.path.exists(REGISTERED_USERS_FILE):
+        with open(REGISTERED_USERS_FILE, "a") as file:
+            file.write(f"{user_id}\n")
+    else:
+        with open(REGISTERED_USERS_FILE, "w") as file:
+            file.write(f"{user_id}\n")
+
+def get_registered_users():
+    if os.path.exists(REGISTERED_USERS_FILE):
+        with open(REGISTERED_USERS_FILE, "r") as file:
+            return file.readlines()
+    return []
